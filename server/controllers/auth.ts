82aa0d71c4d06cfbadbcde7e4199e2099d03c084
Vytautas Povilaitis <vytux@me.com>
2023-07-25 13:14:33 +0300

    Adds refresh token action

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -2,19 +2,37 @@ import { Controller } from 'framework/controller';
 import { actionPost } from 'framework/action';
 import { z } from 'zod';
 
+const JWTSchema = z.string().describe('JWT');
+
 export default Controller('/auth', {
 
   index: actionPost(
     {
+      summary: 'Authenticate',
       body: {
         username: z.string(),
         password: z.string(),
       },
-      output: z.string().describe('JWT'),
+      output: z.object({ token: JWTSchema, refreshToken: JWTSchema }),
     },
     ({ username, password }) => {
-      return `nope ${username} x ${password}`;
+      return {
+        token: 'one',
+        refreshToken: 'two',
+      };
     },
   ),
+
+  refresh: actionPost(
+    {
+      summary: 'Refresh authentification token',
+      route: '/refresh',
+      headers: { refreshToken: JWTSchema },
+      output: JWTSchema,
+    },
+    ({ refreshToken }) => {
+      return `new ${refreshToken}`;
+    },
+  )
 
 });
