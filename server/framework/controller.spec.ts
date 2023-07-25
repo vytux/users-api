2a4543e0734b1ac5de0e991739dd6e1c24876bea
Vytautas Povilaitis <vytux@me.com>
2023-07-25 17:41:05 +0300

    Adds controller test

---
+++ server/framework/controller.spec.ts
@@ -0,0 +1,21 @@
+import Action from 'framework/action';
+import { Controller } from 'framework/controller';
+import expect from 'expect';
+import { z } from 'zod';
+
+describe('controller', () => {
+  const toUpper = (data: { text: string}) => data.text.toUpperCase();
+
+  it('extends action routes', () => {
+    const result = Controller('/test', {
+      toUpper: Action.get({
+        route: '/toUpper/:text',
+        params: { text: z.string() },
+      }, toUpper),
+    });
+
+    expect(result.toUpper.method).toBe('GET');
+    expect(result.toUpper.route).toBe('/test/toUpper/:text');
+    expect(result.toUpper({ text: 'John' })).toBe('JOHN');
+  });
+});
